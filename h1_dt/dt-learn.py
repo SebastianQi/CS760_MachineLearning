5e7df69c2ef83f403aa971094b2386361da0817d
Qihong Lu <lvqihong1992@gmail.com>
2016-09-19 16:41:45 -0500

    start hw1 - DT-ID3

---
+++ h1_dt/dt-learn.py
@@ -0,0 +1,8 @@
+import numpy as np
+import sys
+
+if len(sys.argv) == 4:
+    inputargs = str(sys.argv)
+    print inputargs
+else:
+    sys.exit('ERROR: This program accepts exactly 3 input arguments.')
