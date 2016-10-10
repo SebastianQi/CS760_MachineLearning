6b61a35ef65109c3628cf48e4e8a4aff063d79de
Qihong Lu <lvqihong1992@gmail.com>
2016-10-10 14:06:31 -0500

    re-org code, fix bug: find K smallest... not the K largest dist ...

--- h2_knn/kNN.py
+++ h2_knn/kNN.py
@@ -1,93 +1,47 @@
 import numpy as np
-import scipy.io.arff as sparff
 import sys
-
 from util import *
 ##################################################################################################
 ############################### HELPER FUNCTIONS #################################################
 ##################################################################################################
-def processInputArgs():
-    # process input arguments
-    if len(sys.argv) == 4:
-        fname_train = str(sys.argv[1])
-        fname_test = str(sys.argv[2])
-        K = int(str(sys.argv[3]))
-    else:
-        sys.exit('ERROR: This program takes input arguments in the following way: '
-                 '\n\tpython kNN.py <train-set-file> <test-set-file> k')
-    return fname_train, fname_test, K
-
-
-def loadData(data_):
-    # read the training data
-    data, metadata = sparff.loadarff(data_)
-    M = len(metadata.names()) - 1
-    N = len(data)
-    # convert data into a matrix form
-    X = np.empty((N, M))
-    X[:] = np.NAN
-    Y = []
-    for n in range(N):
-        for m in range(M):
-            X[n,m] = data[n][m]
-        Y.append(data[n][M])
-    return X, np.array(Y), metadata
-
-def showDataInfo(metadata):
-    M = len(metadata.names()) - 1
-    for m in range(M):
-        print("X_%d = <%s>" % (m, metadata.names()[m]))
-    print ("Y = <%s> is <%s>" % (metadata.names()[-1], metadata.types()[-1]))
-
-
-
-
-def getKLargestElements(array, K):
-    ind_largestK = np.argpartition(array, -K)[-K:]
-    return ind_largestK
-
 
 def getMajorityClass(y_partial):
     [counts1, counts2] = [0,0]
-
     for y in y_partial:
         if y == y_range[0]:
             counts1 += 1
-        else:
+        elif y == y_range[1]:
             counts2 += 1
-
+        else:
+            raise
     if counts1 >= counts2:
         return y_range[0]
     else:
         return y_range[1]
-    # TODO : handle tie case
-
+    # TODO : handle tie
 
 
 def kNN_classify_l2(x_test, X_train, Y_train, K):
-    (N_train, M) = X_train.shape
+    N_train = X_train.shape[0]
     distances = np.zeros(N_train,)
-
+    # compute d(x_test, X_train), where d is l2 distance function
     for n in range(N_train):
         distances[n] = np.linalg.norm(x_test - X_train[n,:])
-
-    idx = getKLargestElements(distances,K)
+    # find k smallest indices
+    idx = np.argpartition(distances, K)[:K]
+    # get majority vote for K nearst neighbours
     prediction = getMajorityClass(Y_train[idx])
     return prediction
 
 
 def printPerformance():
-    # predict the label of y
-    N_test = X_test.shape[0]
     count = 0
-    for n in range(N_test):
+    # predict y, for all rows in X_Test
+    for n in range(X_test.shape[0]):
         Y_predict = kNN_classify_l2(X_test[n,], X_train, Y_train, K)
-
         if Y_predict == Y_test[n]:
             count += 1
-
         print('%d: Actual: %s Predicted: %s' %(n+1, Y_test[n], Y_predict))
-
     print('Number of correctly classified: %d Total number of test instances: %d'
           % (count,len(Y_test)))
 
@@ -101,7 +55,6 @@ fname_train, fname_test, K = processInputArgs()
 X_train, Y_train, metadata = loadData(fname_train)
 X_test, Y_test, _ = loadData(fname_test)
 y_range = metadata[metadata.names()[-1]][1]
-
 
 printPerformance()
 
--- h2_knn/util.py
+++ h2_knn/util.py
@@ -1,11 +1,45 @@
+import numpy as np
+import scipy.io.arff as sparff
+import sys
+
 TYPE_NOMINAL = 'nominal'
 TYPE_NUMERIC = 'numeric'
 NAME_RESPONSE = 'response'
 
-def printAllFeatures(metadata, feature_vals):
-    for i in range(len(feature_vals)):
-        featurevalues = "[......]"
-        # featurevalues = str(feature_vals[i])
-    print "%d\t%8s\t%s\t%s" % \
-          (i, metadata.names()[i], metadata.types()[i], featurevalues)
-    print 70*"-"
+
+def processInputArgs():
+    # process input arguments
+    if len(sys.argv) == 4:
+        fname_train = str(sys.argv[1])
+        fname_test = str(sys.argv[2])
+        K = int(str(sys.argv[3]))
+    else:
+        sys.exit('ERROR: This program takes input arguments in the following way: '
+                 '\n\tpython kNN.py <train-set-file> <test-set-file> k')
+    return fname_train, fname_test, K
+
+
+def loadData(data_):
+    # read the training data
+    data, metadata = sparff.loadarff(data_)
+    M = len(metadata.names()) - 1
+    N = len(data)
+    # convert data into a matrix form
+    X = np.empty((N, M))
+    X[:] = np.NAN
+    Y = []
+    for n in range(N):
+        for m in range(M):
+            X[n,m] = data[n][m]
+        Y.append(data[n][M])
+    return X, np.array(Y), metadata
+
+
+
+def showDataInfo(metadata):
+    M = len(metadata.names()) - 1
+    for m in range(M):
+        print("X_%d = <%s>" % (m, metadata.names()[m]))
+    print ("Y = <%s> is <%s>" % (metadata.names()[-1], metadata.types()[-1]))
+
+
