5c269c412c7928a3472c7ba26ab7e710d05ca6f6
Qihong Lu <lvqihong1992@gmail.com>
2016-10-12 00:00:43 -0500

    handle multi-class classification

--- h2_knn/kNN_alg.py
+++ h2_knn/kNN_alg.py
@@ -1,20 +1,16 @@
 import numpy as np
 import sys
 
-def getMajorityClass(y_partial, Y_range):
-    [counts1, counts2] = [0,0]
-    for y in y_partial:
-        if y == Y_range[0]:
-            counts1 += 1
-        elif y == Y_range[1]:
-            counts2 += 1
-        else:
-            raise
-    if counts1 >= counts2:
-        return Y_range[0]
-    else:
-        return Y_range[1]
-    # TODO : handle tie
+def getMajorityClass(Y_predicted, Y_range):
+    # initialize the counts
+    counts = {}
+    for y_label in Y_range:
+        counts[y_label] = 0
+    # accumulate the counts
+    for y_pred in Y_predicted:
+        counts[y_pred] += 1
+    # return the majority vote
+    return max(counts, key=counts.get)
 
 
 def kNN_classify_l2(x_test, X_train, Y_train, Y_range, K):
@@ -39,35 +35,42 @@ def testModel(X_train, Y_train, X_test, Y_test, Y_range, K, printResults = True)
     print('Parameter: K = %d' % K)
     count = 0
     mean_abs_error = 0
+
+    if X_test.shape[0] != len(Y_test):
+        print 'WTF'
+
+    Y_HAT = []
     # predict y, for all rows in X_Test
-    for n in range(X_test.shape[0]):
-        Y_predict = kNN_classify_l2(X_test[n,], X_train, Y_train, Y_range, K)
+    for n in range(len(Y_test)):
+        Y_hat = kNN_classify_l2(X_test[n,], X_train, Y_train, Y_range, K)
+        Y_HAT.append(Y_hat)
         # for regression, aggregate mean absolute error
         if Y_range == None:
-            mean_abs_error += sum(abs(Y_predict - Y_test[n]))
+            mean_abs_error += abs(Y_hat - Y_test[n])
         # for binary classification, aggregate the count of correctly classified instance
         else:
-            if Y_predict == Y_test[n]:
+            if Y_hat == Y_test[n]:
                 count += 1
 
         if printResults:
-            print('%d: Actual: %s Predicted: %s' %(n+1, Y_test[n], Y_predict))
+            print('%d: Actual: %s Predicted: %s' %(n+1, Y_test[n], Y_hat))
 
     if Y_range == None:
-        print('Cumulative MAE = ' % mean_abs_error)
-        return 1.0 * mean_abs_error / len(Y_test)
+        mean_abs_error = 1.0 * mean_abs_error / len(Y_test)
+        print('MAE = %f' % mean_abs_error)
+        return mean_abs_error
     else:
         print('Number of correctly classified: %d Total number of test instances: %d'
           % (count,len(Y_test)))
-        error = 1 - 1.0 * count/len(Y_test)
-        return error
+        accuracy = 1.0 * count/len(Y_test)
+        return accuracy, Y_HAT
 
 
 def tuneModel_loov(X_train, Y_train, x_test, y_test, Y_range, K):
     y_predict = kNN_classify_l2(x_test, X_train, Y_train, Y_range, K)
     # for regression, return true or false
     if Y_range == None:
-        abs_error = sum(abs(y_predict - y_test))
+        abs_error = abs(y_predict - y_test)
         return abs_error
     # for binary classification case, return mean absolute error
     else:
