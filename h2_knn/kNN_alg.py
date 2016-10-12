8f009024d19802a96f10be64d3a92e6f772796e1
Qihong Lu <lvqihong1992@gmail.com>
2016-10-12 14:02:41 -0500

    tie handling, pick the nearest n

--- h2_knn/kNN_alg.py
+++ h2_knn/kNN_alg.py
@@ -1,7 +1,7 @@
 import numpy as np
 import sys
 
-def getMajorityClass(Y_predicted, Y_range):
+def getMajorityClass(Y_predicted, Y_range, nearest_label):
     # initialize the counts
     counts = {}
     for y_label in Y_range:
@@ -9,8 +9,14 @@ def getMajorityClass(Y_predicted, Y_range):
     # accumulate the counts
     for y_pred in Y_predicted:
         counts[y_pred] += 1
-    # return the majority vote
-    return max(counts, key=counts.get)
+
+    # # return the majority vote
+    majorityVotes = [k for k, val in counts.items() if val == max(counts.values())]
+    if len(majorityVotes) > 1:
+        return nearest_label
+    else:
+        return majorityVotes[0]
+
 
 
 def kNN_classify_l2(x_test, X_train, Y_train, Y_range, K):
@@ -21,13 +27,14 @@ def kNN_classify_l2(x_test, X_train, Y_train, Y_range, K):
         distances[n] = np.linalg.norm(x_test - X_train[n,:])
     # find k smallest indices
     idx = np.argpartition(distances, K)[:K]
+    nearest_idx = np.argpartition(distances, 1)[:1]
 
-    # for regression, average k nearst elements
     if Y_range == None:
+        # for regression, average k nearst elements
         prediction = np.mean(Y_train[idx])
-    # for binary classification  get majority vote for K nearst neighbours
     else:
-        prediction = getMajorityClass(Y_train[idx], Y_range)
+        # for binary classification  get majority vote for K nearst neighbours
+        prediction = getMajorityClass(Y_train[idx], Y_range, Y_train[nearest_idx][0])
     return prediction
 
 
@@ -35,10 +42,6 @@ def testModel(X_train, Y_train, X_test, Y_test, Y_range, K, printResults = True)
     print('Parameter: K = %d' % K)
     count = 0
     mean_abs_error = 0
-
-    if X_test.shape[0] != len(Y_test):
-        print 'WTF'
-
     Y_HAT = []
     # predict y, for all rows in X_Test
     for n in range(len(Y_test)):
@@ -53,7 +56,7 @@ def testModel(X_train, Y_train, X_test, Y_test, Y_range, K, printResults = True)
                 count += 1
 
         if printResults:
-            print('%d: Actual: %s Predicted: %s' %(n+1, Y_test[n], Y_hat))
+            print('%d: Actual: %s Predicted: %s' % (n+1, Y_test[n], Y_hat))
 
     if Y_range == None:
         mean_abs_error = 1.0 * mean_abs_error / len(Y_test)
@@ -61,19 +64,20 @@ def testModel(X_train, Y_train, X_test, Y_test, Y_range, K, printResults = True)
         return mean_abs_error
     else:
         print('Number of correctly classified: %d Total number of test instances: %d'
-          % (count,len(Y_test)))
-        accuracy = 1.0 * count/len(Y_test)
+          % (count, len(Y_test)))
+        accuracy = 1.0 * count / len(Y_test)
         return accuracy, Y_HAT
 
 
 def tuneModel_loov(X_train, Y_train, x_test, y_test, Y_range, K):
     y_predict = kNN_classify_l2(x_test, X_train, Y_train, Y_range, K)
-    # for regression, return true or false
+
     if Y_range == None:
+        # for regression, return true or false
         abs_error = abs(y_predict - y_test)
         return abs_error
-    # for binary classification case, return mean absolute error
     else:
+        # for binary classification case, return mean absolute error
         if y_predict == y_test:
             return 1
         else:
