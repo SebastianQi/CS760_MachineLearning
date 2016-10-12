b7a19a25af76c65ee0eb450370629910fca116c0
Qihong Lu <lvqihong1992@gmail.com>
2016-10-12 14:02:19 -0500

    have to do this because a strange error...

--- h2_knn/kNN_hwPart2_2.py
+++ h2_knn/kNN_hwPart2_2.py
@@ -12,12 +12,14 @@ X_test, Y_test, _ = loadData(fname_test)
 Y_range = metadata[metadata.names()[-1]][1]
 
 Ks = [1, 2, 3, 5, 10]
-accuracy_test = np.zeros(len(Ks),)
+accuracies_test = []
 for i in range(len(Ks)):
-    accuracy_test[i], _ = testModel(X_train, Y_train, X_test, Y_test, Y_range, Ks[i], False)
+    accuracy = testModel(X_train, Y_train, X_test, Y_test, Y_range, Ks[i], False)
+    accuracies_test.append(accuracy)
+
 
 plt.figure(1)
-plt.plot(Ks, accuracy_test)
+plt.plot(Ks, accuracies_test)
 
 plt.title('%s' % fname_test)
 plt.ylabel('Test set mean absolute error')
