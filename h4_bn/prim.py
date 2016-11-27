547588b642f0a9693d345e755be315598fbf3f97
Qihong Lu <lvqihong1992@gmail.com>
2016-11-27 11:17:22 -0600

    find max spanning tree by prim

--- h4_bn/prim.py
+++ h4_bn/prim.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 # prim - minimum spanning tree
 # Tim Wilson, 2-25-2002
 
@@ -8,6 +9,7 @@ def adjacent(A, u):
     for x in range(len(A)):
         if A[u][x] > 0 and x <> u:
             L.insert(0,x)
+    L = sorted(L) # sort by increasing order, s.t. earlier vertex will be preferred
     return L
 
 #Q = min queue
@@ -17,46 +19,45 @@ def extractMin(Q):
     return q
 
 #Q = min queue, V = vertex list
-def decreaseKey(Q, K):
+def sort_increaseKey(Q, K):
     for i in range(len(Q)):
         for j in range(len(Q)):
-            if K[Q[i]] < K[Q[j]]:
+            if K[Q[i]] > K[Q[j]]:
                 s = Q[i]
                 Q[i] = Q[j]
                 Q[j] = s
 
 #V = vertex list, A = adjacency list, r = root
-def prim(A):
+def findMaxSpanningTree_prim(A):
+    V = range(np.shape(A)[0])
     # initialize and set each value of the array P (pi) to none
     # pi holds the parent of u, so P(v)=u means u is the parent of v
-    V = range(np.shape(A)[0])
-    P=[None]*len(V)
-    # initialize and set each value of the array K (key) to some large number (simulate infinity)
-    K = [float('inf')]*len(V)
+    P = [None]*len(V)
+    # initialize and set each value of the array K (key) to inf
+    K = [-float('inf')]*len(V)
 
     # initialize the min queue and fill it with all vertices in V
-    Q=[0]*len(V)
-    for u in range(len(Q)):
-        Q[u] = V[u]
+    Q = V
 
     # set the key of the root (the 1st node) to 0
     K[0] = 0
-    decreaseKey(Q, K)    # maintain the min queue
+    sort_increaseKey(Q, K)    # maintain the min queue
 
     # loop while the min queue is not empty
     while len(Q) > 0:
         u = extractMin(Q)    # pop the first vertex off the min queue
 
         # loop through the vertices adjacent to u
-        Adj = adjacent(A, u)
-        for v in Adj:
+        adjVertex = adjacent(A, u)
+
+        for v in adjVertex:
             w = A[u][v]    # get the weight of the edge uv
 
             # proceed if v is in Q and the weight of uv is less than v's key
-            if Q.count(v)>0 and w < K[v]:
+            if Q.count(v)> 0 and w > K[v]:
                 # set v's parent to u
                 P[v] = u
                 # v's key to the weight of uv
                 K[v] = w
-                decreaseKey(Q, K)    # maintain the min queue
+                sort_increaseKey(Q, K)    # maintain the min queue
     return P
