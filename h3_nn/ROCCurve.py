0c9c2351ee1392fbe604c508b3aade98d6457357
Qihong Lu <lvqihong1992@gmail.com>
2016-12-03 16:30:06 -0600

    nothing

--- h3_nn/ROCCurve.py
+++ h3_nn/ROCCurve.py
@@ -22,43 +22,43 @@ weights = trainModel(X_train, Y_train, N_HIDDEN, LRATE, NUM_EPOCHS, 1)
 TP, TN, FP, FN, Y_hat = testModel(X_test, Y_test, weights, 1)
 
 
-def plotROC(target, confidence):
-    # sort (target, prediction) pair
-    targ_pred = sorted(zip(target, confidence), key=lambda x: x[1])
-    targ_pred = np.array(targ_pred)
-    target = targ_pred[:, 0]
-    confidence = targ_pred[:, 1]
-    # pre allocate
-    num_pos = np.sum(target == 1)
-    num_neg = np.sum(target == 0)
-    TP, FP, last_TP = 0, 0, 0
-    TPRs,FPRs = [],[]
-
-    for i in range(1,len(target)):
-        if (confidence[i] != confidence[i-1]) and (target[i] == 0) and (TP > last_TP):
-            FPR = 1.0 * FP / num_neg
-            TPR = 1.0 * TP / num_pos
-            FPRs.append(FPR)
-            TPRs.append(TPR)
-            last_TP = TP
-
-        if target[i] == 1:
-            TP +=1
-        else:
-            FP +=1
-
-    FPR = 1.0 * FP / num_neg
-    TPR = 1.0 * TP / num_pos
-    FPRs.append(FPR)
-    TPRs.append(TPR)
-
-    # plot ROC curve
-    plt.figure(1)
-    LW = 2.0
-    plt.plot(FPRs, TPRs, marker='x', linewidth=LW)
-    plt.title('%s' % fname_test)
-    plt.ylabel('True Positive Rate'); plt.xlabel('False Positive Rate')
-    plt.show()
+# def plotROC(target, confidence):
+#     # sort (target, prediction) pair
+#     targ_pred = sorted(zip(target, confidence), key=lambda x: x[1])
+#     targ_pred = np.array(targ_pred)
+#     target = targ_pred[:, 0]
+#     confidence = targ_pred[:, 1]
+#     # pre allocate
+#     num_pos = np.sum(target == 1)
+#     num_neg = np.sum(target == 0)
+#     TP, FP, last_TP = 0, 0, 0
+#     TPRs,FPRs = [],[]
+#
+#     for i in range(1,len(target)):
+#         if (confidence[i] != confidence[i-1]) and (target[i] == 0) and (TP > last_TP):
+#             FPR = 1.0 * FP / num_neg
+#             TPR = 1.0 * TP / num_pos
+#             FPRs.append(FPR)
+#             TPRs.append(TPR)
+#             last_TP = TP
+#
+#         if target[i] == 1:
+#             TP +=1
+#         else:
+#             FP +=1
+#
+#     FPR = 1.0 * FP / num_neg
+#     TPR = 1.0 * TP / num_pos
+#     FPRs.append(FPR)
+#     TPRs.append(TPR)
+#
+#     # plot ROC curve
+#     plt.figure(1)
+#     LW = 2.0
+#     plt.plot(FPRs, TPRs, marker='x', linewidth=LW)
+#     plt.title('%s' % fname_test)
+#     plt.ylabel('True Positive Rate'); plt.xlabel('False Positive Rate')
+#     plt.show()
 
 # plotROC(Y_test, Y_hat)
 
